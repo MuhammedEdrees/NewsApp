b4515dc744198fcaa61c58e03594c66b14ffbdf4
Edrees <mohamededrees234@hotmail.com>
2023-09-04 14:37:55 +0300

    error fixes

--- app/src/main/java/com/edrees/newsapp/ui/categories/CategoriesFragment.kt
+++ app/src/main/java/com/edrees/newsapp/ui/categories/CategoriesFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.edrees.newsapp.databinding.FragmentGalleryBinding
+import com.edrees.newsapp.databinding.FragmentCategoriesBinding
 
 class CategoriesFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentCategoriesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class CategoriesFragment : Fragment() {
         val galleryViewModel =
             ViewModelProvider(this).get(CategoriesViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentCategoriesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
