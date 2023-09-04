a19a08e2b3e93da344f6302fb50fd169baea0a1c
Edrees <mohamededrees234@hotmail.com>
2023-09-04 14:30:49 +0300

    updated nav drawer menu items

--- app/src/main/java/com/edrees/newsapp/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/edrees/newsapp/ui/search/SearchViewModel.kt
@@ -1,10 +1,10 @@
-package com.edrees.newsapp.ui.slideshow
+package com.edrees.newsapp.ui.search
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class SearchViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is slideshow Fragment"
