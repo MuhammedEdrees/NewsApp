07eb93b85360f5fba95156ac85992fdd60ceaa5f
Edrees <mohamededrees234@hotmail.com>
2023-09-14 01:27:24 +0300

    added SettingFragment and implemented some basic settings

--- app/src/main/java/com/edrees/newsapp/MainActivity.kt
+++ app/src/main/java/com/edrees/newsapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.edrees.newsapp
 
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,10 +12,14 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.preference.PreferenceManager
 import com.edrees.newsapp.databinding.ActivityMainBinding
+import com.edrees.newsapp.util.Constants
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
@@ -37,12 +43,53 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        //load settings
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        Constants.setCountry(sharedPreferences.getString("country", "us")!!)
+        Constants.setLanguage(sharedPreferences.getString("language", "en")!!)
+        when(sharedPreferences.getString("dark_mode", "")){
+            "default" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+            "on" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            "off" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+//        when(sharedPreferences.getString("font_size", "")){
+//            "small"-> {
+//                setTheme(R.style.FontSizeSmall)
+//            }
+//            "medium"-> {
+//                setTheme(R.style.FontSizeMedium)
+//            }
+//            "large"-> {
+//                setTheme(R.style.FontSizeLarge)
+//            }
+//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_settings -> {
+                findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.settingsFragment)
+                true
+            }
+            R.id.action_about ->{
+                findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.aboutFragment)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
